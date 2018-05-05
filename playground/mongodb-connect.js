5d3f8ce9372d0f3a1fa4e496b4360ffd542cd9a7
Arun S D <sdarockz@gmail.com>
2018-05-05 22:46:44 +0530

    Initial Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,27 @@
+const {MongoClient} = require('mongodb');
+
+// To Connect To MongoDB
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
+    if(err){
+        // To throw Error
+        return console.log('Unable to conect to MongoDB server',err);   
+    }
+    console.log('Connected To Server'); 
+    // To Create New DB 
+    const db = client.db('TodosApp');
+    // To Insert New Data Into A Collection
+    db.collection('Todos').insertOne({
+        text:'first Todo',
+        completed: false
+        // CallBack Function To Handle Err and to return the value entered.
+    },(Err,result)=>{
+        if(err){
+            return console.log('Unable to insert Todo',err);
+        }
+        console.log(JSON.stringify(result.ops,null,2));
+        
+    });
+
+    // To close the db after everything's over.
+    client.close();
+});
