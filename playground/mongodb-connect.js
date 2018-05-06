d6f22a0625adfe6aa3de8a8fa77fdf1b0722b34b
Arun S D <sdarockz@gmail.com>
2018-05-06 12:44:02 +0530

    Added Connect And Find

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -10,8 +10,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
     // To Create New DB 
     const db = client.db('TodosApp');
     // To Insert New Data Into A Collection
+    
     db.collection('Todos').insertOne({
-        text:'first Todo',
+        text:'second Todo',
         completed: false
         // CallBack Function To Handle Err and to return the value entered.
     },(Err,result)=>{
@@ -21,6 +22,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
         console.log(JSON.stringify(result.ops,null,2));
         
     });
+    
+    // db.collection('Users').insertOne({
+    //     name:'Arun S D',
+    //     age : 23,
+    //     location : 'Chennai'
+    //     // CallBack Function To Handle Err and to return the value entered.
+    // },(Err,result)=>{
+    //     if(err){
+    //         return console.log('Unable to insert Todo',err);
+    //     }
+    //     // @ops is to get the value inserted from the result
+    //     console.log(JSON.stringify(result.ops,null,2));
+        
+    // });
 
     // To close the db after everything's over.
     client.close();
---
+++ playground/mongodb-find.js
@@ -0,0 +1,30 @@
+const {MongoClient , ObjectID} = require('mongodb');
+
+// To Connect To MongoDB
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
+    if(err){
+        // To throw Error
+        return console.log('Unable to conect to MongoDB server',err);   
+    }
+    console.log('Connected To Server'); 
+    // To Create New DB 
+    const db = client.db('TodosApp');
+
+    // To Fetch All The Data In The MongoDB
+    db.collection('Todos').find().toArray()
+    .then(docs=> console.log(JSON.stringify(docs,null,2)))
+    .catch(err => console.log('error Occured',err));
+
+    // To Fetch And Query The Data In The MongoDB
+    db.collection('Todos').find({completed : false}).toArray()
+        .then(docs => console.log(JSON.stringify(docs, null, 2)))
+        .catch(err => console.log('error Occured', err));
+
+    // To Query With id From the mongoDB
+    db.collection('Todos').find({ _id: new ObjectID('5aede71a44e3bee3acfef361')}).toArray()
+        .then(docs => console.log(JSON.stringify(docs, null, 2)))
+        .catch(err => console.log('error Occured', err));
+
+    // To close the db after everything's over.
+    client.close();
+});
