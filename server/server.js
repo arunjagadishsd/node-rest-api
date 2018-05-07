78a43e8ca48ec64af84588c80f9e7c08df0fb783
Arun SD <arunjagadishsd@gmail.com>
2018-05-07 17:12:32 +0530

    Test Post /todos route

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {mongoose} = require('./db');
 
-const {Todo,User} = require('../models');
+const {Todo,User} = require('./models');
 
 let app = express();
 
@@ -12,9 +12,11 @@ app.post('/todos', (req, res) => {
     let {text} = req.body;
 
     let todo = new Todo({text});
-    todo.save().then(doc => res.send(doc)).catch(e => res.send(e));
+    todo.save().then(doc => res.send(doc)).catch(e => res.status(400).send(e));
 });
 
 app.listen(3000, () => {
     console.log('Server Started At 3000');
 });
+
+module.exports = {app};
