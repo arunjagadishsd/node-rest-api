02ca96620ff445ca0586355fe2f0ee3d374a748e
Arun SD <arunjagadishsd@gmail.com>
2018-05-07 17:20:25 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -15,6 +15,12 @@ app.post('/todos', (req, res) => {
     todo.save().then(doc => res.send(doc)).catch(e => res.status(400).send(e));
 });
 
+app.get('/todos',(req,res)=>{
+    Todo.find().then(todos=>{
+        res.send({todos})
+    }).catch(e=>res.send({error}));
+});
+
 app.listen(3000, () => {
     console.log('Server Started At 3000');
 });
